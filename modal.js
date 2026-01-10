05d458af833a0aefbac273aba7018d132d27a789
rKiws <90979503+cNatanael@users.noreply.github.com>
2026-01-10 13:37:05 -0600

    Add files via upload

---
+++ modal.js
@@ -0,0 +1,44 @@
+    const modal = document.getElementById("modal");
+const openModal = document.getElementById("openModal");
+const closeModal = document.getElementById("closeModal");
+
+openModal.addEventListener("click", () => {
+  modal.classList.add("active");
+});
+
+closeModal.addEventListener("click", () => {
+  modal.classList.remove("active");
+});
+
+modal.addEventListener("click", (e) => {
+  if (e.target === modal) {
+    modal.classList.remove("active");
+  }
+});
+
+
+openModal.addEventListener("click", () => {
+  modal.classList.add("active");
+  document.body.style.overflow = "hidden";
+});
+
+closeModal.addEventListener("click", () => {
+  modal.classList.remove("active");
+  document.body.style.overflow = "";
+});
+
+modal.addEventListener("click", (e) => {
+  if (e.target === modal) {
+    modal.classList.remove("active");
+    document.body.style.overflow = "";
+  }
+});
+
+
+document.querySelector(".confirmacion").addEventListener("click", () => {
+  const phone = "50370405416"; // sin + ni espacios
+  const message = "Hola, confirmo mi asistencia a la boda 💍";
+
+  const url = `https://wa.me/${phone}?text=${encodeURIComponent(message)}`;
+  window.open(url, "_blank");
+});
